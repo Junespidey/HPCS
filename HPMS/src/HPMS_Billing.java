05b6620d1bb6da9ca3b33a0de78e7f156b8b1a32
Junespidey <68677207+Junespidey@users.noreply.github.com>
2022-04-19 19:00:11 -0400

    Update HPMS_Billing.java

--- HPMS/src/HPMS_Billing.java
+++ HPMS/src/HPMS_Billing.java
@@ -1,20 +1,41 @@
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class HPMS_Billing {
 
 	
 // public HTTP Call method  
+	public static HttpRequest Getter() {
+		HttpURLConnection connection;
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request   = HttpRequest.newBuilder().uri(URI.create("http://happypets-1867210382.us-east-1.elb.amazonaws.com/api/billing")).build();
+		client.sendAsync(request,HttpResponse.BodyHandlers.ofString())//tell server to recieve response as a string 
+        .thenApply(HttpResponse::body) // returns response gofuture data type used for aysnchronous programming
+        .thenAccept(System.out::println)// once we receive the response we want to apply the ::body method to previous results
+                                        //:: means it's a lamda , once that is done we use this body to print out, using printline method
+        .join();
+		return request;
+	}
 	
 	
 	
 	public static void runBilling() {
 		
 	}
-	public String getID() {
-		return id;
+	//public String getID() {
+		//return id;
+		
+		
+	
+	public static void main(String[]args) {
+		System.out.println(Getter());
+		
 		
 		
 	}
-	
 
 
 }
